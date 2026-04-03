e53e07c6c928ea7410ce6341c168b6eb62b89726
Demetrius Rauck <demetrius@melagenlabs.com>
2026-04-03 09:59:27 -0700

    space

--- melagen_read_sensors.py
+++ melagen_read_sensors.py
@@ -99,7 +99,6 @@ def ads_read_reg(bus, reg):
         val = list(read)[0]
         print(f"ADS RD 0x{reg:02X}->0x{val:02X} PASS")
         return val
-
     except Exception as e:
         print("ADS RD FAIL", e)
         return None
@@ -115,7 +114,6 @@ def ads_read_adc(bus):
         dose = (voltage / A) ** (1.0 / B)
     	# V = A * Dose^B  => Dose = (V/A)^(1/B)
         return raw, voltage, dose
-
     except Exception as e:
         print("ADC READ FAIL", e)
         return None, None, None
@@ -147,9 +145,7 @@ def update_io_expander(bus, port0, port1):
     return tca_write(bus, REG_OUTPUT_PORT0, port0) & tca_write(bus, REG_OUTPUT_PORT1, port1)
 
 def tca9539_config(bus):
-
     print("\nInitializing TCA9539")
-
     return tca_write(bus, REG_OUTPUT_PORT0, 0x00) & \
     tca_write(bus, REG_OUTPUT_PORT1, 0x00) & \
     tca_write(bus, REG_CONFIG_PORT0, 0x00) & \
@@ -167,27 +163,20 @@ def enable_r2(bus):
     update_io_expander(bus, R2_PORT0, R2_PORT1)
     time.sleep(0.2)
 
-
 def disable_all(bus):
-
     print("\nDisabling sensors")
-
     update_io_expander(bus, 0x00, 0x00)
-
 
 # ==========================================================
 # ADS7138 INIT
 # ==========================================================
 def ads7138_init(bus):
-
     print("\nInitializing ADS7138")
-
     ads_write_reg(bus, REG_PIN_CFG, 0x00)
     ads_write_reg(bus, REG_SEQUENCE_CFG, 0x00)
     ads_write_reg(bus, REG_GENERAL_CFG, 0x00)
     ads_write_reg(bus, REG_OPMODE_CFG, 0x00)
     ads_write_reg(bus, REG_DATA_CFG, 0x10)
-
     time.sleep(0.05)
 
 # ==========================================================
@@ -198,21 +187,16 @@ def read_all_channels(bus, csv_writer, sensor_group):
 	print("\nADC READINGS")
 	results=[]
 	for ch in range(2,7):
-
 		ads_write_reg(bus, REG_CHANNEL_SEL, ch)
 		confirm = ads_read_reg(bus, REG_CHANNEL_SEL)
-
 		if confirm != ch:
 			results.append(f"Change to CH{ch-1}:FAIL")
 			continue	
-
 		raw, voltage, dose = ads_read_adc(bus)
-
 		if raw is None:
 			results.append(f"CH{ch-1}:FAIL")
 		else:
-			results.append(f"CH{ch-1}:{raw} ({voltage:.3f}V) ({dose:.3f}rad)")
-			
+			results.append(f"CH{ch-1}:{raw} ({voltage:.3f}V) ({dose:.3f}rad)")	
 			csv_writer.writerow([
 				datetime.now().isoformat(),
 				sensor_group,
@@ -221,18 +205,14 @@ def read_all_channels(bus, csv_writer, sensor_group):
 				voltage,
 				dose
 			])
-
 		time.sleep(0.01)
 	print("|".join(results))
 
 # ==========================================================
 # Main Sequence
 # ==========================================================
-
 file_exists = os.path.isfile(CSV_FILE)
-
 with open(CSV_FILE, "a", newline="") as f:
-
     writer = csv.writer(f)
     # write header only if file did not exist
     if not file_exists:
